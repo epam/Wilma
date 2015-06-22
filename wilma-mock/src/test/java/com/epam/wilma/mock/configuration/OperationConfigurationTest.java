37ac02d5c8c5103edca308917fc2a444427f8090
TPinter <thommster@gmail.com>
2015-06-22 15:56:00 +0200

    created operation configuration class

---
+++ wilma-mock/src/test/java/com/epam/wilma/mock/configuration/OperationConfigurationTest.java
@@ -0,0 +1,119 @@
+package com.epam.wilma.mock.configuration;
+
+/*==========================================================================
+ Copyright 2015 EPAM Systems
+
+ This file is part of Wilma.
+
+ Wilma is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+
+ Wilma is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with Wilma.  If not, see <http://www.gnu.org/licenses/>.
+ ===========================================================================*/
+
+import static com.epam.wilma.mock.domain.OperationMode.PROXY;
+import static com.epam.wilma.mock.domain.OperationMode.STUB;
+import static com.epam.wilma.mock.domain.OperationMode.WILMA;
+import static junit.framework.Assert.assertEquals;
+import static org.mockito.Matchers.anyString;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import static org.testng.Assert.assertFalse;
+import static org.testng.Assert.assertTrue;
+
+import org.json.JSONObject;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import com.epam.wilma.mock.domain.WilmaMockConfig;
+import com.epam.wilma.mock.http.WilmaHttpClient;
+import com.google.common.base.Optional;
+
+/**
+ * Unit test for {@link OperationConfiguration}.
+ *
+ * @author Tamas_Pinter
+ *
+ */
+public class OperationConfigurationTest {
+
+    private static final String HOST = "host";
+    private static final Integer PORT = 1;
+    private static final String OPERATION_MODE_STATUS_URL = "http://host:1/config/public/switch/status";
+    private static final String OPERATION_MODE_SETTER_URL_WILMA = "http://host:1/config/admin/switch/wilma";
+    private static final String OPERATION_MODE_SETTER_URL_STUB = "http://host:1/config/admin/switch/stub";
+    private static final String OPERATION_MODE_SETTER_URL_PROXY = "http://host:1/config/admin/switch/proxy";
+    private static final String JSON_STRING = "{\"JSON\": \"string\"}";
+
+    private static final JSONObject EMPTY_JSON_OBJECT = new JSONObject();
+
+    @Mock
+    private WilmaHttpClient client;
+
+    private OperationConfiguration operationConfiguration;
+
+    @BeforeMethod
+    public void init() {
+        MockitoAnnotations.initMocks(this);
+
+        WilmaMockConfig config = createMockConfig();
+        operationConfiguration = new OperationConfiguration(config, client);
+    }
+
+    @Test(expectedExceptions = IllegalArgumentException.class)
+    public void shouldThrowExceptionWhenConfigIsMissing() {
+        new OperationConfiguration(null);
+    }
+
+    @Test
+    public void shouldReturnEmptyJSONObjectIfClientReturnsOptionalAbsent() {
+        when(client.sendGetterRequest(OPERATION_MODE_STATUS_URL)).thenReturn(Optional.<String>absent());
+
+        JSONObject result = operationConfiguration.getOperationMode();
+
+        assertTrue(EMPTY_JSON_OBJECT.similar(result));
+        assertEquals(EMPTY_JSON_OBJECT.length(), result.length());
+        verify(client, never()).sendSetterRequest(anyString());
+    }
+
+    @Test
+    public void shouldReturnJSONObjectWithProperValue() {
+        when(client.sendGetterRequest(OPERATION_MODE_STATUS_URL)).thenReturn(Optional.of(JSON_STRING));
+
+        JSONObject result = operationConfiguration.getOperationMode();
+
+        assertTrue(new JSONObject(JSON_STRING).similar(result), "The two JSON objects should be similar.");
+        verify(client, never()).sendSetterRequest(anyString());
+    }
+
+    @Test
+    public void shouldReturnWithProperBooleanValueForSetterRequest() {
+        when(client.sendSetterRequest(OPERATION_MODE_SETTER_URL_WILMA)).thenReturn(true);
+        when(client.sendSetterRequest(OPERATION_MODE_SETTER_URL_STUB)).thenReturn(false);
+        when(client.sendSetterRequest(OPERATION_MODE_SETTER_URL_PROXY)).thenReturn(true);
+
+        assertTrue(operationConfiguration.setOperationMode(WILMA));
+        assertTrue(operationConfiguration.setOperationMode(PROXY));
+        assertFalse(operationConfiguration.setOperationMode(STUB));
+        verify(client, never()).sendGetterRequest(anyString());
+    }
+
+    private WilmaMockConfig createMockConfig() {
+        return WilmaMockConfig.getBuilder()
+                .withHost(HOST)
+                .withPort(PORT)
+                .build();
+    }
+
+}
