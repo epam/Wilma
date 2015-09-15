580bf18c0997dfaedd621fb9584568b9ab7e84a7
root <root@debian>
2015-09-15 23:48:46 +0200

    Wilma image creation is updated, now contains the start_wilma script. Several ENV variables are available to configure it as necessary. Not yet finalized (Wilma + Wilma MS creation is not ready, and using volumes to save app log is not addressed.

--- config/docker/start_wilma.sh
+++ config/docker/start_wilma.sh
@@ -2,12 +2,15 @@
 
 WILMA_BUILD=wilma-1.1.73.jar
 WILMA_PID_FILE=/tmp/wilma.pid
-WILMA_MX_SIZE=
-WILMA_KEPYSTORE=
-#WILMA_KESYTORE=-Djavax.net.ssl.keyStore=certificate/your.jks
-WILMA_KEYSTORE_PASSWORD=
-#WILMA_KEYSTORE_PASSWORD=-Djavax.net.ssl.keyStorePassword=password_for_your_jks
-WILMA_START_CMD="nohup java -Dcom.sun.management.jmxremote.port=9011 -Dcom.sun.management.jmxremote.authenticate=false -Dcom.sun.management.jmxremote.ssl=false $WILMA_MX_SIZE $WILMA_KEYSTORE $WILMA_KEYSTORE_PASSWORD -jar $WILMA_BUILD wilma.conf.properties & echo \$! > $WILMA_PID_FILE &"
+#WILMA_MX_SIZE is an external environment variable, for example: -Xmx8G
+#WILMA_KEPYSTORE is an external environment variable, for example: -Djavax.net.ssl.keyStore=certificate/your.jks
+#WILMA_CONFIGURATION is an external environment variable, usually: wilma.conf.properties
+#WILMA_KEYSTORE_PASSWORD=is an external environment variable, example: -Djavax.net.ssl.keyStorePassword=password_for_your_jks
+#WILMA_JMX_PORT - if empty, no JMX used, otherwise the JMX port is fully opened
+if [ -n "$WILMA_JMX_PORT" ]; then
+    WILMA_JMX="-Dcom.sun.management.jmxremote.port=$WILMA_JMX_PORT -Dcom.sun.management.jmxremote.authenticate=false -Dcom.sun.management.jmxremote.ssl=false"
+fi
+WILMA_START_CMD="java $WILMA_JMX $WILMA_MX_SIZE $WILMA_KEYSTORE $WILMA_KEYSTORE_PASSWORD -jar $WILMA_BUILD $WILMA_CONFIGURATION"
 
 echo
 echo "Welcome to dockerized Wilma!"
@@ -16,9 +19,8 @@ echo
 echo "[INFO] Docker image IP information:"
 cat /etc/hosts
 
-echo "[INFO] Starting Wilma Application..."
+echo
+echo "[INFO] Starting Wilma Application, using command: $WILMA_START_CMD"
+echo
 cd wilma
 eval ${WILMA_START_CMD}
-cd -
-echo "[INFO] Wilma Application was started, process id is $(cat ${WILMA_PID_FILE})"
-
