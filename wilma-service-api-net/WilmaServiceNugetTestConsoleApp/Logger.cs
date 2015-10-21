0941aad14de1310eb89b318d4adb8240984ae3a3
szgerg <szocir@gmail.com>
2015-10-21 16:17:44 +0200

    Test Console App done, with nuget package

---
+++ wilma-service-api-net/WilmaServiceNugetTestConsoleApp/Logger.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using epam.wilma_service_api;
+
+namespace WilmaServiceNugetTestConsoleApp
+{
+    public class Logger : ILogger
+    {
+        public void Debug(string format, params object[] prs)
+        {
+            Console.WriteLine(format, prs);
+        }
+
+        public void Warning(string format, params object[] prs)
+        {
+            Console.WriteLine(format, prs);
+        }
+
+        public void Error(string format, params object[] prs)
+        {
+            Console.WriteLine(format, prs);
+        }
+
+        public void Info(string format, params object[] prs)
+        {
+            Console.WriteLine(format, prs);
+        }
+    }
+}
--- wilma-service-api-net/WilmaServiceNugetTestConsoleApp/Program.cs
+++ wilma-service-api-net/WilmaServiceNugetTestConsoleApp/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
+using epam.wilma_service_api;
 
 namespace WilmaServiceNugetTestConsoleApp
 {
@@ -10,44 +12,44 @@ namespace WilmaServiceNugetTestConsoleApp
     {
         static void Main(string[] args)
         {
-            //var wsConf = new WilmaServiceConfig("EPHUBUDW2039T1.budapest.epam.com", 1234);
-            //var ws = new WilmaService(wsConf, new Logger());
+            var wsConf = new WilmaServiceConfig("EPHUBUDW2039T1.budapest.epam.com", 1234);
+            var ws = new WilmaService(wsConf, new Logger());
 
-            //ws.GetVersionInformationAsync().ContinueWith(res => { Console.WriteLine(res.Result); });
-            //ws.GetActualLoadInformationAsync().ContinueWith(res => { Console.WriteLine(res.Result); });
-            //ws.GetMessageLoggingStatusAsync().ContinueWith(res => { Console.WriteLine(res.Result); });
+            ws.GetVersionInformationAsync().ContinueWith(res => { Console.WriteLine(res.Result); });
+            ws.GetActualLoadInformationAsync().ContinueWith(res => { Console.WriteLine(res.Result); });
+            ws.GetMessageLoggingStatusAsync().ContinueWith(res => { Console.WriteLine(res.Result); });
 
-            //ws.SetMessageLoggingStatusAsync(WilmaService.MessageLoggingControlStatus.On).ContinueWith(res => { if (res.Result) { ws.GetMessageLoggingStatusAsync().ContinueWith(res1 => { Console.WriteLine(res1.Result); }); } });
+            ws.SetMessageLoggingStatusAsync(WilmaService.MessageLoggingControlStatus.On).ContinueWith(res => { if (res.Result) { ws.GetMessageLoggingStatusAsync().ContinueWith(res1 => { Console.WriteLine(res1.Result); }); } });
 
-            //ws.SetOperationModeAsync(WilmaService.OperationModes.WILMA).ContinueWith(res1 => { ws.GetOperationModeAsync().ContinueWith(res => { Console.WriteLine(res.Result); }); });
+            ws.SetOperationModeAsync(WilmaService.OperationModes.WILMA).ContinueWith(res1 => { ws.GetOperationModeAsync().ContinueWith(res => { Console.WriteLine(res.Result); }); });
 
-            //ws.SetLocalhostBlockingStatusAsync(WilmaService.LocalhostControlStatuses.On).ContinueWith(res1 => { ws.GetLocalhostBlockingStatusAsync().ContinueWith(res => { Console.WriteLine(res.Result); }); });
+            ws.SetLocalhostBlockingStatusAsync(WilmaService.LocalhostControlStatuses.On).ContinueWith(res1 => { ws.GetLocalhostBlockingStatusAsync().ContinueWith(res => { Console.WriteLine(res.Result); }); });
 
-            //ws.GetStubConfigInformationAsync().ContinueWith(res => { Console.WriteLine(res.Result); });
+            ws.GetStubConfigInformationAsync().ContinueWith(res => { Console.WriteLine(res.Result); });
 
-            //ws.ChangeStubConfigStatusAsync("EPAMNEWS", WilmaService.StubConfigStatus.Enabled).ContinueWith(res => { Console.WriteLine(res.Result); });
-            //ws.ChangeStubConfigOrderAsync("EPAMNEWS", WilmaService.StubConfigOrder.Up).ContinueWith(res => { Console.WriteLine(res.Result); });
-            //ws.DropStubConfigAsync("EPAMNEWS").ContinueWith(res => { Console.WriteLine(res.Result); });
+            ws.ChangeStubConfigStatusAsync("EPAMNEWS", WilmaService.StubConfigStatus.Enabled).ContinueWith(res => { Console.WriteLine(res.Result); });
+            ws.ChangeStubConfigOrderAsync("EPAMNEWS", WilmaService.StubConfigOrder.Up).ContinueWith(res => { Console.WriteLine(res.Result); });
+            ws.DropStubConfigAsync("EPAMNEWS").ContinueWith(res => { Console.WriteLine(res.Result); });
 
-            //ws.PersistActualStubConfigAsync().ContinueWith(res => { Console.WriteLine(res.Result); });
+            ws.PersistActualStubConfigAsync().ContinueWith(res => { Console.WriteLine(res.Result); });
 
-            //UploadFile(@"c:\wilmaaa\proba.json", ws.UploadConditionCheckerAsync).ContinueWith(res => { Console.WriteLine(res.Result); });
-            //UploadFile(@"c:\wilmaaa\proba.json", ws.UploadTemplateAsync).ContinueWith(res => { Console.WriteLine(res.Result); });
-            //UploadFile(@"c:\wilmaaa\proba.json", ws.UploadTemplateFormatterAsync).ContinueWith(res => { Console.WriteLine(res.Result); });
-            //UploadFile(@"c:\wilmaaa\proba.json", ws.UploadStubConfigurationAsync).ContinueWith(res => { Console.WriteLine(res.Result); });
+            UploadFile(@"c:\wilmaaa\proba.json", ws.UploadConditionCheckerAsync).ContinueWith(res => { Console.WriteLine(res.Result); });
+            UploadFile(@"c:\wilmaaa\proba.json", ws.UploadTemplateAsync).ContinueWith(res => { Console.WriteLine(res.Result); });
+            UploadFile(@"c:\wilmaaa\proba.json", ws.UploadTemplateFormatterAsync).ContinueWith(res => { Console.WriteLine(res.Result); });
+            UploadFile(@"c:\wilmaaa\proba.json", ws.UploadStubConfigurationAsync).ContinueWith(res => { Console.WriteLine(res.Result); });
 
-            //// ws.ShutdownApplicationAsync().ContinueWith(res => { Console.WriteLine("Wilma shuted down: {0}", res.Result);});
+            // ws.ShutdownApplicationAsync().ContinueWith(res => { Console.WriteLine("Wilma shuted down: {0}", res.Result);});
 
-            //Console.ReadLine();
+            Console.ReadLine();
         }
 
-        //private static async Task<bool> UploadFile(string filePath, Func<string, Stream, Task<bool>> func)
-        //{
-        //    using (var fs = File.OpenRead(filePath))
-        //    {
-        //        var res = await func(Path.GetFileName(filePath), fs);
-        //        return res;
-        //    }
-        //}
+        private static async Task<bool> UploadFile(string filePath, Func<string, Stream, Task<bool>> func)
+        {
+            using (var fs = File.OpenRead(filePath))
+            {
+                var res = await func(Path.GetFileName(filePath), fs);
+                return res;
+            }
+        }
     }
 }
