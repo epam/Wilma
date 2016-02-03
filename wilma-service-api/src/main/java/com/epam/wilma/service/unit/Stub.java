89a78e5605f1103dce0430385f429230efd75009
tkohegyi <tkohegyi@vnet.hu>
2016-02-03 22:06:57 +0100

    initial non working version - just the first steps - don't expect anything

---
+++ wilma-service-api/src/main/java/com/epam/wilma/service/unit/Stub.java
@@ -0,0 +1,12 @@
+package com.epam.wilma.service.unit;
+
+import com.epam.wilma.service.unit.request.RequestCondition;
+import com.epam.wilma.service.unit.response.ResponseDescriptor;
+
+/**
+ * Created by tkohegyi on 2016. 01. 27..
+ */
+public class Stub {
+    private RequestCondition requestCondition;
+    private ResponseDescriptor responseDescriptor;
+}
---
+++ wilma-service-api/src/main/java/com/epam/wilma/service/unit/response/ResponseDescriptor.java
@@ -0,0 +1,7 @@
+package com.epam.wilma.service.unit.response;
+
+/**
+ * Created by tkohegyi on 2016. 01. 27..
+ */
+public class ResponseDescriptor {
+}
