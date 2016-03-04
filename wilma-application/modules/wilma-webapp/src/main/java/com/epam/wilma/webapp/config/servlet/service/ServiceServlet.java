29d7bbd9c058b20617947da8318642442d5a9af3
tkohegyi2 <tkohegyi2@gmail.com>
2016-03-05 00:16:37 +0100

    start implementing #77 - it will be a very useful feature

--- wilma-application/modules/wilma-webapp/src/main/java/com/epam/wilma/webapp/config/servlet/service/ServiceServlet.java
+++ wilma-application/modules/wilma-webapp/src/main/java/com/epam/wilma/webapp/config/servlet/service/ServiceServlet.java
@@ -20,6 +20,8 @@ along with Wilma.  If not, see <http://www.gnu.org/licenses/>.
 ===========================================================================*/
 
 import com.epam.wilma.common.helper.UniqueIdGenerator;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 import javax.servlet.ServletException;
@@ -37,20 +39,33 @@ import java.io.PrintWriter;
 @Component
 public class ServiceServlet extends HttpServlet {
     private static final String LEADING_TEXT = "/public/service/";
+    private final Logger logger = LoggerFactory.getLogger(ServiceServlet.class);
+
 
     @Override
     protected void doGet(final HttpServletRequest req, final HttpServletResponse resp) throws ServletException, IOException {
-        resp.setContentType("application/json");
-        PrintWriter out = resp.getWriter();
+        //first identify the requested service
         int requestedServicePosition = req.getRequestURI().indexOf(LEADING_TEXT) + LEADING_TEXT.length();
         String requestedService = req.getRequestURI().substring(requestedServicePosition);
+        logger.info("Service call to: " + requestedService + ", method: " + req.getMethod());
+
+        //set the default answer
+        resp.setContentType("application/json");
         String response = "{\"unknownServiceCall\":\"" + requestedService + "\"}";
         resp.setStatus(HttpServletResponse.SC_NOT_FOUND);
+
+        //call the built-in service, as necessary
         if ("uniqueId".equalsIgnoreCase(requestedService) && "get".equalsIgnoreCase(req.getMethod())) {
             // get a new unique id
             response = getUniqueId();
             resp.setStatus(HttpServletResponse.SC_OK);
         }
+
+        //call further registered services
+        // to be implemented, something response = calls to methods of the service
+
+        //write the answer back
+        PrintWriter out = resp.getWriter();
         out.write(response);
         out.flush();
         out.close();
