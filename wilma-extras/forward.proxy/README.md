60643248c07226d4f0ea52954bc5d693cbbd147d
Tamas Kohegyi <tkohegyi2@gmail.com>
2017-10-18 17:38:11 +0200

    initial version of forwrad proxy - of course don't expect that it is working - as it is under construction

---
+++ wilma-extras/forward.proxy/README.md
@@ -0,0 +1,22 @@
+Wilma Example - Forward Proxy
+=============================
+
+Purpose of this example
+---------------------------------------
+
+Implementation
+---------------------------------------
+
+Important Settings
+---------------------------------------
+Don't forget to **enable** the interceptors! Without that, the Short Circuit won't work properly (no message will be cached at all).
+
+REST Interface of Forward Proxy plugin
+---------------------------------------
+Note: In order to list the available services, use this call:
+```
+GET or POST http://localhost:1234/config/public/services/
+```
+See more information on using external service calls from Wilma plugins [here](https://github.com/epam/Wilma/wiki/Service-extensions-in-Plugins).
+Also, in the examples below, it is assumed that the default configuration is in use and Wilma is used on `localhost`,
+therefore the Wilma `<standardExternalServiceUrl>` is: `http://localhost:1234/config/public/services/`.
---
+++ wilma-extras/forward.proxy/src/main/java/com/epam/wilma/extras/forwardproxy/ForwardProxyService.java
@@ -0,0 +1,170 @@
+package com.epam.wilma.extras.forwardproxy;
+/*==========================================================================
+Copyright 2013-2017 EPAM Systems
+
+This file is part of Wilma.
+
+Wilma is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+Wilma is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Wilma.  If not, see <http://www.gnu.org/licenses/>.
+===========================================================================*/
+
+import com.epam.wilma.webapp.service.external.ExternalWilmaService;
+import com.google.common.collect.Sets;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.Set;
+
+/**
+ * This class provides the Circuit Breaker Service calls by using the ExternalWilmaService extension point of Wilma.
+ *
+ * @author tkohegyi
+ */
+class ForwardProxyService extends ForwardProxyFileHandler implements ExternalWilmaService {
+
+    private static final String HANDLED_SERVICE = "/forward-proxy";
+
+    /**
+     * ExternalWilmaService method implementation - provides the list of requests, this service will handle.
+     * Note that both GAT and DELETE are supported.
+     *
+     * @return with the set of handled services.
+     */
+    @Override
+    public Set<String> getHandlers() {
+        return Sets.newHashSet(
+                this.getClass().getSimpleName() + HANDLED_SERVICE
+        );
+    }
+
+    /**
+     * ExternalWilmaService method implementation - entry point to handle the request by the external service.
+     *
+     * @param httpServletRequest  is the original request
+     * @param request             is the request string itself (part of the URL, focusing on the requested service)
+     * @param httpServletResponse is the response object
+     * @return with the body of the response (need to set response code in httpServletResponse object)
+     */
+    @Override
+    public String handleRequest(HttpServletRequest httpServletRequest, String request, HttpServletResponse httpServletResponse) {
+        String myMethod = httpServletRequest.getMethod();
+        String myService = (this.getClass().getSimpleName() + HANDLED_SERVICE).toLowerCase();
+        boolean myCall = request.toLowerCase().startsWith(myService);
+
+        //set default response
+        String response = "{ \"unknownServiceCall\": \"" + myMethod + " " + request + "\" }";
+        httpServletResponse.setStatus(HttpServletResponse.SC_NOT_FOUND);
+
+        //handle basic call (without query string)
+        if (myCall && httpServletRequest.getQueryString() == null) {
+            //get the map, or delete whole map or delete entry from map
+            response = handleBasicCall(myMethod, httpServletResponse, httpServletRequest.getPathInfo());
+        }
+
+        //handle complex calls (with query string as folder)
+        if (myCall && httpServletRequest.getQueryString() != null && httpServletRequest.getQueryString().length() > 0) {
+            String folder = httpServletRequest.getParameter("folder");
+            if (folder != null && folder.length() > 0) {
+                //save (post) and load (get) map
+                response = handleComplexCall(myMethod, folder, httpServletResponse);
+            }
+        }
+        return response;
+    }
+
+    /**
+     * Method that handles request to save and load the Short Circuit Map.
+     *
+     * @param myMethod            POST (for Save) and GET (for Load) and DELETE for a selected
+     * @param folder              is the folder to be used, or the identifier of the entry to be deleted
+     * @param httpServletResponse is the response object
+     * @return with the response body (and with the updated httpServletResponse object
+     */
+    private String handleComplexCall(String myMethod, String folder, HttpServletResponse httpServletResponse) {
+        String response = null;
+        if ("post".equalsIgnoreCase(myMethod)) {
+            //save map (to files) (post + forwardProxy?folder=...)
+            response = saveMap(folder, httpServletResponse);
+        }
+        if ("get".equalsIgnoreCase(myMethod)) {
+            //load map (from files) (get + forwardProxy?folder=....)
+            loadMap(folder);
+            response = getForwardPoxyMap(httpServletResponse);
+        }
+        return response;
+    }
+
+
+    private String handleBasicCall(String myMethod, HttpServletResponse httpServletResponse, String path) {
+        String response = null;
+        if ("get".equalsIgnoreCase(myMethod)) {
+            //get the forward proxy map
+            response = getForwardPoxyMap(httpServletResponse);
+        }
+        if ("delete".equalsIgnoreCase(myMethod)) {
+            //first detect if we have basic path or we have a specified id in it
+            int index = path.lastIndexOf("/");
+            String idStr = path.substring(index + 1);
+            int id;
+            try {
+                id = Integer.parseInt(idStr);
+                //remove a specific map entry
+                String[] keySet = FORWARD_PROXY_MAP.keySet().toArray(new String[FORWARD_PROXY_MAP.size()]);
+                if (keySet.length > id) {
+                    //we can delete it
+                    String entryKey = keySet[id];
+                    FORWARD_PROXY_MAP.remove(entryKey);
+                    response = getForwardPoxyMap(httpServletResponse);
+                } else {
+                    //resource cannot be deleted
+                    response = "{ \"Cannot found specified entry in Forward Proxy\": \"" + id + "\" }";
+                    httpServletResponse.setStatus(HttpServletResponse.SC_NOT_FOUND);
+                }
+            } catch (NumberFormatException e) {
+                //it is not a problem, we should delete all
+                //invalidate map (remove all from map) (circuits + delete)
+                synchronized (GUARD) {
+                    FORWARD_PROXY_MAP.clear();
+                }
+                response = getForwardPoxyMap(httpServletResponse);
+            }
+        }
+        return response;
+    }
+
+    /**
+     * Gets the actual status of the Circuit Breaker Map.
+     *
+     * @param httpServletResponse is the response object
+     * @return with the response body (and with the updated httpServletResponse object
+     */
+    private String getForwardPoxyMap(HttpServletResponse httpServletResponse) {
+        StringBuilder response = new StringBuilder("{\n  \"circuitBreakerMap\": [\n");
+        if (!FORWARD_PROXY_MAP.isEmpty()) {
+            String[] keySet = FORWARD_PROXY_MAP.keySet().toArray(new String[FORWARD_PROXY_MAP.size()]);
+            for (int i = 0; i < keySet.length; i++) {
+                String entryKey = keySet[i];
+                ForwardProxyInformation circuitBreakerInformation = FORWARD_PROXY_MAP.get(entryKey);
+                response.append(circuitBreakerInformation.toString());
+                if (i < keySet.length - 1) {
+                    response.append(",\n");
+                }
+            }
+            response.append("\n");
+        }
+        response.append("  ]\n}\n");
+        httpServletResponse.setStatus(HttpServletResponse.SC_OK);
+        return response.toString();
+    }
+
+}
