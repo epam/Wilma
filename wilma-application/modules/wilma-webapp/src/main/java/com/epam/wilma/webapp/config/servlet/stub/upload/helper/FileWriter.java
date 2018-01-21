e9e022b9157ad4ce77107c6a7f4c52614ded2367
tkohegyi2 <tkohegyi2@gmail.com>
2018-01-21 19:08:39 +0100

    fix sonar issue considered as blocker

--- wilma-application/modules/wilma-webapp/src/main/java/com/epam/wilma/webapp/config/servlet/stub/upload/helper/FileWriter.java
+++ wilma-application/modules/wilma-webapp/src/main/java/com/epam/wilma/webapp/config/servlet/stub/upload/helper/FileWriter.java
@@ -18,24 +18,23 @@ You should have received a copy of the GNU General Public License
 along with Wilma.  If not, see <http://www.gnu.org/licenses/>.
 ===========================================================================*/
 
+import com.epam.wilma.common.helper.FileFactory;
+import com.epam.wilma.webapp.config.servlet.helper.InputStreamUtil;
+import com.epam.wilma.webapp.domain.exception.CannotUploadExternalResourceException;
+import com.epam.wilma.webapp.stub.servlet.helper.ByteArrayInputStreamFactory;
+import org.apache.commons.io.IOUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 
-import org.apache.commons.io.IOUtils;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
-
-import com.epam.wilma.common.helper.FileFactory;
-import com.epam.wilma.webapp.config.servlet.helper.InputStreamUtil;
-import com.epam.wilma.webapp.domain.exception.CannotUploadExternalResourceException;
-import com.epam.wilma.webapp.stub.servlet.helper.ByteArrayInputStreamFactory;
-
 /**
  * File writer class.
- * @author Tunde_Kovacs
  *
+ * @author Tunde_Kovacs
  */
 @Component
 public class FileWriter {
@@ -55,39 +54,45 @@ public class FileWriter {
 
     /**
      * Writes the content of an inputstream to a file.
-     * @param inputStream the {@link InputStream} that will be written to the file
-     * @param fileName the name of the file the <tt>inputStream</tt> will be written
+     *
+     * @param inputStream      the {@link InputStream} that will be written to the file
+     * @param fileName         the name of the file the <tt>inputStream</tt> will be written
      * @param exceptionMessage the message that a {@link CannotUploadExternalResourceException} should be thrown with
      */
     public void write(final InputStream inputStream, final String fileName, final String exceptionMessage) {
         String filePath = fileName;
         InputStream inputStreamToWrite = inputStream;
-        if (fileName.endsWith(".class")) {
-            InputStreamCopier inputStreamCopier = new InputStreamCopier(inputStream, fileName, exceptionMessage);
-            InputStream byteArrayInputStream = inputStreamCopier.createCopy();
-            filePath = classFilePathAssembler.createFilePath(byteArrayInputStream, fileName, exceptionMessage);
-            inputStreamToWrite = inputStreamCopier.createCopy();
-        } else if (fileName.endsWith(".jar")) {
-            InputStreamCopier inputStreamCopier = new InputStreamCopier(inputStream, fileName, exceptionMessage);
-            InputStream byteArrayInputStream = inputStreamCopier.createCopy();
-            jarValidator.validateInputStream(byteArrayInputStream);
-            inputStreamToWrite = inputStreamCopier.createCopy();
-        }
-
-        File newClass = fileFactory.createFile(filePath);
-        //if file is in a package, create folder structure
-        String directories = newClass.getParent();
-        if (directories != null && !directories.isEmpty()) {
-            (fileFactory.createFile(directories)).mkdirs();
-        }
         try {
-            // if file doesnt exists, then create it
+            boolean isClass = fileName.endsWith(".class");
+            boolean isJar = fileName.endsWith(".jar");
+            if (isClass) {
+                InputStreamCopier inputStreamCopier = new InputStreamCopier(inputStream, fileName, exceptionMessage);
+                InputStream byteArrayInputStream = inputStreamCopier.createCopy();
+                filePath = classFilePathAssembler.createFilePath(byteArrayInputStream, fileName, exceptionMessage);
+                inputStreamToWrite = inputStreamCopier.createCopy();
+            } else if (isJar) {
+                InputStreamCopier inputStreamCopier = new InputStreamCopier(inputStream, fileName, exceptionMessage);
+                InputStream byteArrayInputStream = inputStreamCopier.createCopy();
+                jarValidator.validateInputStream(byteArrayInputStream);
+                inputStreamToWrite = inputStreamCopier.createCopy();
+            }
+
+            File newClass = fileFactory.createFile(filePath);
+            //if file is in a package, create folder structure
+            String directories = newClass.getParent();
+            if (directories != null && !directories.isEmpty()) {
+                (fileFactory.createFile(directories)).mkdirs();
+            }
+            // if file doesn't exist, then create it
             if (!newClass.exists()) {
                 newClass.createNewFile();
             }
             FileOutputStream fos = fileOutputStreamFactory.createFileOutputStream(newClass);
             IOUtils.copy(inputStreamToWrite, fos);
             fos.close();
+            if (isClass || isJar) {
+                inputStreamToWrite.close();
+            }
         } catch (IOException e) {
             throw new CannotUploadExternalResourceException(exceptionMessage + filePath, e);
         }
