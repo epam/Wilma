16c03abda5fd98fe497c3cbdb13e7cf6bd81da75
tkohegyi <tamas_kohegyi@epam.com>
2021-05-16 22:55:03 +0200

    issue #111 is still problematic, waiting for bountycastle fix

--- config/environment/start-wilma-environment.sh
+++ config/environment/start-wilma-environment.sh
@@ -8,7 +8,7 @@ sleep 10s
 
 # start wilma
 cd /opt/wilma/testenv/wilma
-nohup /usr/bin/java -Xmx512m -jar wilma.jar wilma.conf.properties > wilma.out 2>&1 &
+nohup /usr/bin/java -Xmx512m -Djdk.tls.namedGroups="secp256r1, secp384r1, ffdhe2048, ffdhe3072" -jar wilma.jar wilma.conf.properties > wilma.out 2>&1 &
 
 # start test-server
 cd /opt/wilma/testenv/test-server
